52bff67a8e12b5dc58c8152dd30342bef5dfbb67
Coolchickenguy <152336774+Coolchickenguy@users.noreply.github.com>
2024-07-03 14:16:36 -0500

    Update extends-classes.d.ts: simplify

--- lib/extends-classes.d.ts
+++ lib/extends-classes.d.ts
@@ -1,12 +1,4 @@
 import MethodMissing from "method-missing";
-type functionType = typeof Function;
-type NonConstructorKeys<value> = ({[key in keyof value]: value[key] extends new () => any ? never : key })[keyof value];
-type NonConstructor<value> = Pick<value, NonConstructorKeys<value>>;
-type getncFromArgs<args> = NonConstructor<(typeof MethodMissing & args[number])["prototype"]>;
-interface output<args> extends NonConstructor<functionType>{
-    new(...opts:any[]): getncFromArgs<args>, 
-    "prototype":getncFromArgs<args>
-    "constructor":(...opts:any) => getncFromArgs<args>
-}
-declare function classes<args>(...args:args):output<args>
+type UnionToIntersection<input> = (input extends any ? (value: input)=>void : never) extends ((value: infer infered)=>void) ? infered : never
+declare function classes<args>(...args:args):{new(...opts:any[]): UnionToIntersection<(typeof MethodMissing | args[number])["prototype"]>} //output<args>
 export = classes;
