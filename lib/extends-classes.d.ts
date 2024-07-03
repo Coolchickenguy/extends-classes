96695b45841e600dc7e3d25bd9e0fab870d8a69e
Coolchickenguy <152336774+Coolchickenguy@users.noreply.github.com>
2024-07-03 13:37:16 -0500

    Update extends-classes.d.ts: allow for classes to see propertys of the classes they are extending

--- lib/extends-classes.d.ts
+++ lib/extends-classes.d.ts
@@ -1,3 +1,12 @@
 import MethodMissing from "method-missing";
-declare function classes<args>(...args:args):typeof MethodMissing & {prototype:args[number]["prototype"]}
+type functionType = typeof Function;
+type NonConstructorKeys<value> = ({[key in keyof value]: value[key] extends new () => any ? never : key })[keyof value];
+type NonConstructor<value> = Pick<value, NonConstructorKeys<value>>;
+type getncFromArgs<args> = NonConstructor<(typeof MethodMissing & args[number])["prototype"]>;
+interface output<args> extends NonConstructor<functionType>{
+    new(...opts:any[]): getncFromArgs<args>, 
+    "prototype":getncFromArgs<args>
+    "constructor":(...opts:any) => getncFromArgs<args>
+}
+declare function classes<args>(...args:args):output<args>
 export = classes;
